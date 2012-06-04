9f0e92f9f84b13f6b3ba3d9678a072d11873a1d6
Ben M. Ward <ben.ward@gmail.com>
2012-06-04 04:21:34 -0600

    Initial Import - Cleanup Pending

---
+++ Server/Java/Server/src/com/foransrealm/programmastercontrol/server/Server.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.foransrealm.programmastercontrol.server;
+
+import com.sun.jersey.api.container.grizzly2.GrizzlyServerFactory;
+import com.sun.jersey.api.core.PackagesResourceConfig;
+import com.sun.jersey.api.core.ResourceConfig;
+import com.sun.jersey.api.json.*;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.glassfish.grizzly.http.server.HttpServer;
+
+/**
+ *
+ * @author Foran
+ */
+public class Server {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            ResourceConfig rc = new PackagesResourceConfig("com.foransrealm.programmastercontrol.server.ws.rest");
+            rc.getProperties().put(JSONConfiguration.FEATURE_POJO_MAPPING, true);
+            HttpServer httpServer = GrizzlyServerFactory.createHttpServer("http://0.0.0.0:12345/", rc);
+            System.in.read();
+            httpServer.stop();
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalArgumentException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NullPointerException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
