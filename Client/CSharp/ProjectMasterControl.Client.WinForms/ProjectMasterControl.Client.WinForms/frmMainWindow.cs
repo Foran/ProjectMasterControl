a5100f075282573671bdf97ea19b024ade8d2131
Ben M. Ward <ben.ward@gmail.com>
2012-06-10 01:07:13 -0600

    Initial import of CS UI Client

---
+++ Client/CSharp/ProjectMasterControl.Client.WinForms/ProjectMasterControl.Client.WinForms/frmMainWindow.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ForansRealm.ProjectMasterControl.Client.WinForms
+{
+    public partial class frmMainWindow : Form
+    {
+        public frmMainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
